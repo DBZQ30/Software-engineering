527438573888f3941c836123eefec9f09866e723
DBZQ30 <96719524+DBZQ30@users.noreply.github.com>
2023-04-12 11:35:12 +0800

    删除了后端代码

--- springboot/src/main/java/com/example/springboot/controller/request/BaseRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.controller.request;
-
-import lombok.Data;
-
-@Data
-public class BaseRequest {
-    private Integer pageNum = 1;
-    private Integer pageSize = 10;
-}
--- springboot/src/main/java/com/example/springboot/controller/request/LoginRequest.java
+++
@@ -1,9 +0,0 @@
-package com.example.springboot.controller.request;
-
-import lombok.Data;
-
-@Data
-public class LoginRequest {
-    private String username;
-    private String password;
-}
