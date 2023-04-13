5f0f5192a36300d75f30c6b3a9b9aaf69f7e99da
DBZQ30 <96719524+DBZQ30@users.noreply.github.com>
2023-04-13 16:45:51 +0800

    更新了一些代码，修改了文件结构

---
+++ src/router/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+const routes = [
+   //  ====== 登录  =====
+  // {
+	//   path:'/',
+	//   name:'login',
+  //     component: () => import('@/views/Login/loginRegister.vue'),
+  // },
+    //  ====== 主页  =====
+    {
+        path: '/',
+        name: 'Layout',
+        component: () => import('@/views/Layout.vue'),
+    },
+    {
+        path: "*",
+        component:() => import('@/views/404.vue')
+    }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
