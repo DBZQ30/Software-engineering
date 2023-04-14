9163b46f1c16310b23f0cd02faae93ae79fc7882
DBZQ30 <96719524+DBZQ30@users.noreply.github.com>
2023-04-14 13:08:23 +0800

    update some code

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped="scoped">
+  #app{
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: #f5f5f5;
+  }
+</style>
---
+++ src/views/404.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class = "bad">
+    <div style="text-align: center">
+      <div style="margin-top: 150px; font-size: 100px; ">404</div>
+      <div style="font-size: 50px">未找到页面</div>
+      <div><el-button type="text" style="margin-top: 20px; font-size: 20px" @click="$router.push('/')">返回主页</el-button></div>
+    </div>
+  </div>
+
+</template>
+
+<style scoped="scoped">
+.bad{
+  background: url("~@/assets/img.png") no-repeat;
+  background-position: center;
+  height:100%;
+  width:100%;
+  background-size:cover;
+  position: fixed;
+}
+</style>
